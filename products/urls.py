2bd4da24f5607eb5382371b9b0315a794ed49917
Tarmo <tsillajoe@gmail.com>
2024-10-07 20:26:15 +0300

    applesauce.html

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("cross/", views.cross_refs, name="cross_refs"),
     path("merchants/", views.merchants, name="merchants"),
     path("query/", views.code_query_form, name="code_query_form"),
+    path("applesauce/", views.applesauce, name="applesauce"),
 ]
