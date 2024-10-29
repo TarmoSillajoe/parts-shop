a5f4b7954030dafae4de9d32908bb64f594ae011
Tarmo <tsillajoe@gmail.com>
2024-10-29 13:43:47 +0200

    autos_xml_invoice.py - converting strings representing numeric values to numeric datatypes

--- products/autos_xml_invoice.py
+++ products/autos_xml_invoice.py
@@ -1,6 +1,12 @@
 import xml
 import xml.etree
 import xml.etree.ElementTree as ET
+import rich
+from pathlib import Path
+
+
+def str_to_float(price: str) -> float:
+    return float(price.replace(",", "."))
 
 
 def get_invoice_rows(filepath) -> list[ET.Element]:
@@ -13,9 +19,27 @@ def code_for(invoice_row: ET.Element) -> str:
     return invoice_row.find("KOD_TOWARU").text
 
 
-def quantity_for(invoice_row: ET.Element):
-    return invoice_row.find("ILE").text
+def quantity_for(invoice_row: ET.Element) -> int:
+    qty: str = invoice_row.find("ILE").text
+    return int(qty)
 
 
 def price_for(invoice_row: ET.Element):
-    return invoice_row.find("TOW_CEN_BON").text
+    price: str = invoice_row.find("TOW_CEN_BON").text
+    return str_to_float(price)
+
+
+if __name__ == "__main__":
+    result = []
+    # filepath = Path(__file__).parent / "tests" / "data" / "autos_invoice.xml"
+    filepath = "/mnt/c/users/tarmos/koik/FUEX_8121_CN_2024.xml"
+    for row in get_invoice_rows(filepath):
+        result.append(
+            {
+                "code": code_for(row),
+                "qty": quantity_for(row),
+                "price": price_for(row),
+            }
+        )
+
+    rich.print(result)
